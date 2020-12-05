e617896c54e5e21a6bf4f35b455f082ff1e9e123
Mel Fabella <mf820am@gmail.com>
2020-12-05 14:50:08 -0800

    created routes and controller

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,13 +1,25 @@
 class TripsController < ApplicationController
+  before_action :set_trip, only: [:show, :edit, :update, :destroy] 
   def index
+    @trips = current_user.trips  
   end
 
   def show
   end
 
   def new
+    @trip = Trip.new 
   end
 
-  def edit
+  def edit 
+  end
+
+  private
+  def set_trip
+    @trip = current_user.trip.find(params[:id])
+  end
+
+  def trip_params
+    params.require(:trip).permit(:name, :duration)
   end
 end
